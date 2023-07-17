cb1166b9c9279e87bc92b45471777b899e73b8f4
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2023-07-17 17:32:12 +0100

    chor: add BankDataSource

---
+++ src/main/kotlin/kt/springboot/tutorial/learningktspringb/datasource/BankDataSource.kt
@@ -0,0 +1,7 @@
+package kt.springboot.tutorial.learningktspringb.datasource
+
+import kt.springboot.tutorial.learningktspringb.model.Bank
+
+interface BankDataSource {
+    fun retrieveBanks(): Collection<Bank>
+}
---
+++ src/main/kotlin/kt/springboot/tutorial/learningktspringb/datasource/mock/MockBankDataSource.kt
@@ -0,0 +1,19 @@
+package kt.springboot.tutorial.learningktspringb.datasource.mock
+
+import kt.springboot.tutorial.learningktspringb.datasource.BankDataSource
+import kt.springboot.tutorial.learningktspringb.model.Bank
+import org.springframework.stereotype.Repository
+
+@Repository
+class MockBankDataSource : BankDataSource {
+
+    val banks = listOf(
+            Bank("1234", 0.1, 1),
+            Bank("2345", 17.1, 2),
+            Bank("7689", 15.1, 5),
+    )
+
+    override fun retrieveBanks(): Collection<Bank> {
+        return retrieveBanks
+    }
+}
