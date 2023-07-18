a7e4d9663f7b266d680b1d0f69fea546dff53568
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2023-07-18 12:05:17 +0100

    chor: wed layer - add get all banks endpoint

---
+++ src/main/kotlin/kt/springboot/tutorial/learningktspringb/controller/BankController.kt
@@ -0,0 +1,16 @@
+package kt.springboot.tutorial.learningktspringb.controller
+
+import kt.springboot.tutorial.learningktspringb.model.Bank
+import kt.springboot.tutorial.learningktspringb.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+
+class BankController (private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+}
